2f3be45514fe26ff0ccc6b9a00a22b2ad35ebaf7
Randy Kern <randy@Randys-MacBook-Air.local>
2025-06-15 15:52:41 -0600

    Initial ui/app framework using tecll

--- cmd/tked/main.go
+++ cmd/tked/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
+
+	"tked/internal/app"
+)
 
 func main() {
-	fmt.Println("Hello world!")
-}
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("Failed to create screen: %v", err)
+	}
 
+	if err := screen.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	quit := func() {
+		// You have to catch panics in a defer, clean up, and
+		// re-raise them - otherwise your application can
+		// die without leaving any diagnostic trace.
+		maybePanic := recover()
+		screen.Fini()
+		if maybePanic != nil {
+			panic(maybePanic)
+		}
+	}
+	defer quit()
+
+	app := app.New()
+	app.Run(screen)
+}
--- internal/app/core.go
+++ internal/app/core.go
@@ -1,4 +1,148 @@
 package app
 
-func foo() {
+import (
+	"fmt"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+type App interface {
+	Run(screen tcell.Screen)
+}
+
+type app struct {
+	/*
+		buffers []Buffer,
+		views []View,
+	*/
+}
+
+func New() App {
+	return &app{}
+}
+
+func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
+	row := y1
+	col := x1
+	for _, r := range text {
+		s.SetContent(col, row, r, nil, style)
+		col++
+		if col >= x2 {
+			row++
+			col = x1
+		}
+		if row > y2 {
+			break
+		}
+	}
+}
+
+func drawBox(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
+	if y2 < y1 {
+		y1, y2 = y2, y1
+	}
+	if x2 < x1 {
+		x1, x2 = x2, x1
+	}
+
+	// Fill background
+	for row := y1; row <= y2; row++ {
+		for col := x1; col <= x2; col++ {
+			s.SetContent(col, row, ' ', nil, style)
+		}
+	}
+
+	// Draw borders
+	for col := x1; col <= x2; col++ {
+		s.SetContent(col, y1, tcell.RuneHLine, nil, style)
+		s.SetContent(col, y2, tcell.RuneHLine, nil, style)
+	}
+	for row := y1 + 1; row < y2; row++ {
+		s.SetContent(x1, row, tcell.RuneVLine, nil, style)
+		s.SetContent(x2, row, tcell.RuneVLine, nil, style)
+	}
+
+	// Only draw corners if necessary
+	if y1 != y2 && x1 != x2 {
+		s.SetContent(x1, y1, tcell.RuneULCorner, nil, style)
+		s.SetContent(x2, y1, tcell.RuneURCorner, nil, style)
+		s.SetContent(x1, y2, tcell.RuneLLCorner, nil, style)
+		s.SetContent(x2, y2, tcell.RuneLRCorner, nil, style)
+	}
+
+	drawText(s, x1+1, y1+1, x2-1, y2-1, style, text)
+}
+
+func drawStatusBar(a *app, s tcell.Screen) {
+	width, height := s.Size()
+	drawText(s, 0, height-1, width-1, height-1, tcell.StyleDefault.Foreground(tcell.ColorWhite), "Status bar")
+}
+
+func (a *app) Run(screen tcell.Screen) {
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorPurple)
+
+	// Initialize screen
+	screen.SetStyle(defStyle)
+	screen.EnableMouse()
+	screen.EnablePaste()
+	screen.Clear()
+
+	// Draw initial boxes
+	drawBox(screen, 1, 1, 42, 7, boxStyle, "Click and drag to draw a box")
+	drawBox(screen, 5, 9, 32, 14, boxStyle, "Press C to reset")
+
+	// Draw initial status bar
+	drawStatusBar(a, screen)
+
+	// Here's how to get the screen size when you need it.
+	// xmax, ymax := s.Size()
+
+	// Here's an example of how to inject a keystroke where it will
+	// be picked up by the next PollEvent call.  Note that the
+	// queue is LIFO, it has a limited length, and PostEvent() can
+	// return an error.
+	// s.PostEvent(tcell.NewEventKey(tcell.KeyRune, rune('a'), 0))
+
+	// Event loop
+	ox, oy := -1, -1
+	for {
+		// Update screen
+		screen.Show()
+
+		// Poll event
+		ev := screen.PollEvent()
+
+		// Process event
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			screen.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				return
+			} else if ev.Key() == tcell.KeyCtrlL {
+				screen.Sync()
+			} else if ev.Rune() == 'C' || ev.Rune() == 'c' {
+				screen.Clear()
+			}
+		case *tcell.EventMouse:
+			x, y := ev.Position()
+
+			switch ev.Buttons() {
+			case tcell.Button1, tcell.Button2:
+				if ox < 0 {
+					ox, oy = x, y // record location when click started
+				}
+
+			case tcell.ButtonNone:
+				if ox >= 0 {
+					label := fmt.Sprintf("%d,%d to %d,%d", ox, oy, x, y)
+					drawBox(screen, ox, oy, x, y, boxStyle, label)
+					ox, oy = -1, -1
+				}
+			}
+
+			drawStatusBar(a, screen)
+		}
+	}
 }
