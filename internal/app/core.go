5fde038ee3e6cf68fb68bf0ba0bcbbf2f41d3285
Randy Kern <randy@Randys-MacBook-Air.local>
2025-06-16 14:20:50 -0600

    Move the cursor when you click the mouse button

--- internal/app/core.go
+++ internal/app/core.go
@@ -32,42 +32,6 @@ func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string
 	}
 }
 
-func drawBox(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
-	if y2 < y1 {
-		y1, y2 = y2, y1
-	}
-	if x2 < x1 {
-		x1, x2 = x2, x1
-	}
-
-	// Fill background
-	for row := y1; row <= y2; row++ {
-		for col := x1; col <= x2; col++ {
-			s.SetContent(col, row, ' ', nil, style)
-		}
-	}
-
-	// Draw borders
-	for col := x1; col <= x2; col++ {
-		s.SetContent(col, y1, tcell.RuneHLine, nil, style)
-		s.SetContent(col, y2, tcell.RuneHLine, nil, style)
-	}
-	for row := y1 + 1; row < y2; row++ {
-		s.SetContent(x1, row, tcell.RuneVLine, nil, style)
-		s.SetContent(x2, row, tcell.RuneVLine, nil, style)
-	}
-
-	// Only draw corners if necessary
-	if y1 != y2 && x1 != x2 {
-		s.SetContent(x1, y1, tcell.RuneULCorner, nil, style)
-		s.SetContent(x2, y1, tcell.RuneURCorner, nil, style)
-		s.SetContent(x1, y2, tcell.RuneLLCorner, nil, style)
-		s.SetContent(x2, y2, tcell.RuneLRCorner, nil, style)
-	}
-
-	drawText(s, x1+1, y1+1, x2-1, y2-1, style, text)
-}
-
 func drawStatusBar(a *app, s tcell.Screen) {
 	filename := "Untitled"
 	cursor := ": 1 1"
@@ -142,7 +106,6 @@ func drawView(v View, s tcell.Screen) {
 
 func (a *app) Run(screen tcell.Screen) {
 	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
-	boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorPurple)
 
 	// Initialize screen
 	screen.SetStyle(defStyle)
@@ -158,17 +121,7 @@ func (a *app) Run(screen tcell.Screen) {
 		drawView(a.views[0], screen)
 	}
 
-	// Here's how to get the screen size when you need it.
-	// xmax, ymax := s.Size()
-
-	// Here's an example of how to inject a keystroke where it will
-	// be picked up by the next PollEvent call.  Note that the
-	// queue is LIFO, it has a limited length, and PostEvent() can
-	// return an error.
-	// s.PostEvent(tcell.NewEventKey(tcell.KeyRune, rune('a'), 0))
-
 	// Event loop
-	ox, oy := -1, -1
 	for {
 		// Update screen
 		screen.Show()
@@ -233,17 +186,9 @@ func (a *app) Run(screen tcell.Screen) {
 			x, y := ev.Position()
 
 			switch ev.Buttons() {
-			case tcell.Button1, tcell.Button2:
-				if ox < 0 {
-					ox, oy = x, y // record location when click started
-				}
-
-			case tcell.ButtonNone:
-				if ox >= 0 {
-					label := fmt.Sprintf("%d,%d to %d,%d", ox, oy, x, y)
-					drawBox(screen, ox, oy, x, y, boxStyle, label)
-					ox, oy = -1, -1
-				}
+			case tcell.Button1:
+				top, left := a.views[0].TopLeft()
+				a.views[0].SetCursor(top+y, left+x)
 			}
 		}
 
