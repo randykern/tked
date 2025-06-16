c8b4136bdbcf9fce33d7a784b93499be321586f8
Randy Kern <randy@Randys-MacBook-Air.local>
2025-06-16 13:01:07 -0600

    Add SetTopLeft method to the view interface

--- internal/app/view.go
+++ internal/app/view.go
@@ -1,8 +1,12 @@
 package app
 
 type View interface {
+	// Buffer returns the buffer that the view is displaying.
 	Buffer() Buffer
+	// TopLeft returns the top row and left column offsets.
 	TopLeft() (int, int)
+	// SetTopLeft updates the view's top row and left column offsets.
+	SetTopLeft(top, left int)
 	// Cursor returns the current cursor position as row and column indexes.
 	Cursor() (int, int)
 	// SetCursor updates the current cursor position.
@@ -25,13 +29,18 @@ func (v *view) TopLeft() (int, int) {
 	return v.top, v.left
 }
 
+func (v *view) SetTopLeft(top, left int) {
+	v.top = max(0, top)
+	v.left = max(0, left)
+}
+
 func (v *view) Cursor() (int, int) {
 	return v.cursorRow, v.cursorCol
 }
 
 func (v *view) SetCursor(row, col int) {
-	v.cursorRow = row
-	v.cursorCol = col
+	v.cursorRow = max(0, row)
+	v.cursorCol = max(0, col)
 }
 
 func NewView(buffer Buffer) View {
