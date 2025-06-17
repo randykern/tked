a99816d37911dce7489cd2e8b19f01ae7aa4ca60
randykern <randy@teamkern.net>
2025-06-16 18:27:23 -0600

    Add StatusBar.Errorf

--- internal/app/statusbar.go
+++ internal/app/statusbar.go
@@ -13,6 +13,10 @@ type StatusBar interface {
 	// Input displays a prompt on the status bar and returns the entered value.
 	// The boolean return is false if the prompt was cancelled with Esc.
 	Input(s tcell.Screen, prompt string) (string, bool)
+	// Error displays an error message on the status bar until a key is pressed.
+	Error(s tcell.Screen, msg string)
+	// Errorf formats the error message and displays it until a key is pressed.
+	Errorf(s tcell.Screen, format string, args ...interface{})
 }
 
 type statusBar struct{}
@@ -79,4 +83,29 @@ func (sb *statusBar) Draw(s tcell.Screen, v View) {
 	width, height := s.Size()
 	drawText(s, 0, height-1, width-1, height-1, tcell.StyleDefault.Foreground(tcell.ColorWhite), filename+dirty)
 	drawText(s, len(filename)+len(dirty), height-1, width-1, height-1, tcell.StyleDefault.Foreground(tcell.ColorWhite), cursor)
+}
+
+// Error displays an error message on the status bar until a key is pressed.
+func (sb *statusBar) Error(s tcell.Screen, msg string) {
+	for {
+		width, height := s.Size()
+		for x := 0; x < width; x++ {
+			s.SetContent(x, height-1, ' ', nil, tcell.StyleDefault)
+		}
+		drawText(s, 0, height-1, width-1, height-1, tcell.StyleDefault.Foreground(tcell.ColorRed), msg)
+		s.Show()
+
+		ev := s.PollEvent()
+		switch ev.(type) {
+		case *tcell.EventKey, *tcell.EventMouse:
+			return
+		case *tcell.EventResize:
+			s.Sync()
+		}
+	}
+}
+
+// Errorf formats the error message and displays it until a key is pressed.
+func (sb *statusBar) Errorf(s tcell.Screen, format string, args ...interface{}) {
+	sb.Error(s, fmt.Sprintf(format, args...))
 }
