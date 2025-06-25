46065c8ac2ee183011cdf993134e16f9e02bcf48
randykern <randy@teamkern.net>
2025-06-25 11:09:48 -0600

    Ensure first file is active on startup

--- cmd/tked/openfiles.go
+++ cmd/tked/openfiles.go
@@ -3,10 +3,17 @@ package main
 import "tked/internal/app"
 
 func openFiles(application app.App, filenames []string) error {
-	for _, filename := range filenames {
+	var firstView app.View
+	for i, filename := range filenames {
 		if err := application.OpenFile(filename); err != nil {
 			return err
 		}
+		if i == 0 {
+			firstView = application.GetCurrentView()
+		}
+	}
+	if firstView != nil {
+		application.SetCurrentView(firstView)
 	}
 	return nil
 }
